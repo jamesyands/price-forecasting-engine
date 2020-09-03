fcb6873bdfd604210c520319970b5f046c3e5e88
jamesyands <wmyaiyj41@gmail.com>
2020-09-03 09:15:58 +0800

    add.module.structure

--- modules/Samples/Sample.py
+++ modules/Samples/Sample.py
@@ -12,11 +12,11 @@ def main(args):
     parser = argparse.ArgumentParser(description='Time-Series-Decomposition Input and Output')
 
     # 算子输入参数
-    parser.add_argument("--input1", type=list, required=True)
+    parser.add_argument("--input1", type=str, required=True)
     parser.add_argument("--parameter1", type=str, required=True)
 
     # 算子输出, 系统会自动分配一个路径, 输出值必须写入这个路径才会被系统识别打包存到S3, 为其他算子输入引用做准备
-    parser.add_argument("--output1", type=list, required=True)
+    parser.add_argument("--output1", type=str, required=True)
     args = parser.parse_args(args)
 
     # 创建输出路径，存放输出文件（路径可以存在，也可以不存在）
